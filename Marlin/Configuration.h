16e4bd4fa9798beade5d30b0d04d3da36931b500
BahaEldin A. Elsayed <bhalddin@hotmail.com>
2018-11-20 22:11:21 +0400

    Change Z Rais probe to 10 during probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -324,7 +324,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define DISABLE_E 		false // For all extruders
 #define DISABLE_INACTIVE_EXTRUDER true //disable only inactive extruders and keep active extruder enabled
 
-#define INVERT_X_DIR 	false     	// for Mendel set to false, for Orca set to true
+#define INVERT_X_DIR 	true     	// for Mendel set to false, for Orca set to true
 #define INVERT_Y_DIR 	false    	// for Mendel set to true, for Orca set to false
 #define INVERT_Z_DIR 	true     	// for Mendel set to false, for Orca set to true
 #define INVERT_E0_DIR 	false		// for direct drive extruder v9 set to true, for geared extruder set to false
@@ -335,8 +335,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 		210
-#define X_MIN_POS 		-20
+#define X_MAX_POS 		200
+#define X_MIN_POS 		0
 #define Y_MAX_POS 		210
 #define Y_MIN_POS 		-5
 #define Z_MAX_POS 		180
@@ -393,17 +393,17 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
-  #define X_PROBE_OFFSET_FROM_EXTRUDER 		-32//-25
-  #define Y_PROBE_OFFSET_FROM_EXTRUDER 		0//-29
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER 		-1//-12.35
+  #define X_PROBE_OFFSET_FROM_EXTRUDER 		-39//-25
+  #define Y_PROBE_OFFSET_FROM_EXTRUDER 		-10//-29
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER 		-6.25//-12.35
 
-  #define Z_RAISE_BEFORE_HOMING 			10       // (in mm) Raise Z before homing (G28) for Probe Clearance.
+  #define Z_RAISE_BEFORE_HOMING 			20       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         			// Be sure you have this distance over your Z_MAX_POS in case
 
   #define XY_TRAVEL_SPEED 					6000         // X and Y axis travel speed between probes, in mm/min
 
   #define Z_RAISE_BEFORE_PROBING 			10    //How much the extruder will be raised before traveling to the first probing point.
-  #define Z_RAISE_BETWEEN_PROBINGS 			2  //How much the extruder will be raised when traveling from between next probing points
+  #define Z_RAISE_BETWEEN_PROBINGS 			10  //How much the extruder will be raised when traveling from between next probing points
 
 
   //If defined, the Probe servo will be turned on only during movement and then turned off to avoid jerk
@@ -444,8 +444,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define HOMING_FEEDRATE				{100*60, 100*60, 2*60, 0}  // set the homing speeds (mm/min)
 
 // default settings
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,	100, 1600,90}  				// default steps per unit for P802
-#define DEFAULT_MAX_FEEDRATE          {400, 400, 4, 100}    			// (mm/sec)
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,	100, 400,90}  				// default steps per unit for P802
+#define DEFAULT_MAX_FEEDRATE          {400, 400, 8, 100}    			// (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {1000,1000,60,5000}    			// X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 #define DEFAULT_ACCELERATION          400    							// X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   							// X, Y, Z and E max acceleration in mm/s^2 for retracts
